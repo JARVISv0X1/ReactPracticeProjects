9171d3901fb396cff70f272a43e733b9fb625291
Karan Khallar <karan.khallar.courses@gmail.com>
2024-02-03 22:57:14 +0530

    Added Spring boot Project for all backend related services

---
+++ SpringBootBackend/crm/src/test/java/com/resturant/crm/CrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.resturant.crm;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
