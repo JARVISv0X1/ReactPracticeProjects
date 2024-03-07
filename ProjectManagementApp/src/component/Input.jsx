31f0e0d8daac967bf8431263f082578f0638a3c0
Karan Khallar <karan.khallar.courses@gmail.com>
2024-03-07 13:41:14 +0530

    fix project bug of input type as date

--- ProjectManagementApp/src/component/Input.jsx
+++ ProjectManagementApp/src/component/Input.jsx
@@ -1,19 +1,33 @@
-export default function Input({ label, textarea, ...props }) {
+import { forwardRef } from "react";
+
+const Input = forwardRef(function Input(
+  { label, date, textarea, ...props },
+  ref
+) {
   const style =
-    "w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:bg-stone-600";
+    "w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-b-4  focus:border-bg-stone-800 focus:bg-stone-200";
 
   return (
     <>
       <p className="flex flex-col gap-1 my-4">
-        <label className="text-sm font-bold uppercase text-stone-500">
+        <label
+          {...props}
+          className="text-sm font-bold uppercase text-stone-500"
+        >
           {label}
         </label>
         {textarea ? (
-          <textarea className={style} {...props}></textarea>
+          <textarea ref={ref} className={style} {...props}></textarea>
         ) : (
-          <input className={style} {...props} type="text" />
+          <input
+            ref={ref}
+            className={style}
+            {...props}
+            type={date ? "date" : "text"}
+          />
         )}
       </p>
     </>
   );
-}
+});
+export default Input;
