8ecbf2c5b32a1c2b557fb3cb932750432d5d0a82
Karan Khallar <karan.khallar.courses@gmail.com>
2024-03-07 13:44:32 +0530

    - Added project selection feature. - Added Project deletion feature ( with the help of filter function)

--- ProjectManagementApp/src/App.jsx
+++ ProjectManagementApp/src/App.jsx
@@ -2,22 +2,109 @@ import { useState } from "react";
 import ProjectSideBar from "./component/ProjectSideBar";
 import NewProject from "./component/NewProject";
 import NoProjectSelected from "./component/NoProjectSelected";
+import ProjectSelected from "./component/ProjectSelected";
 
 function App() {
-  const [addProject, setAddProject] = useState(0);
+  // const [projectData, setProjectData] = useState({
+  //   title: "",
+  //   description: "",
+  //   dueDate: "",
+  // });
+  const [projectState, setProjectState] = useState({
+    selectedProjectId: undefined,
+    projects: [],
+  });
 
-  function handleAddProjectButn() {
-    setAddProject((prev) => prev + 1);
+  function handleStartAddProject() {
+    setProjectState((prev) => {
+      return {
+        ...prev,
+        selectedProjectId: null,
+      };
+    });
   }
+
+  function addNewProject(data) {
+    setProjectState((prevState) => {
+      const newProject = {
+        ...data,
+        id: Math.random(),
+      };
+      return {
+        ...prevState,
+        projects: [...prevState.projects, newProject],
+        selectedProjectId: undefined,
+      };
+    });
+  }
+
+  function cancleProject() {
+    setProjectState((prev) => {
+      return {
+        ...prev,
+        selectedProjectId: undefined,
+      };
+    });
+  }
+  function deleteProject(id) {
+    setProjectState((prev) => {
+      const remaningProject = prev.projects.filter(
+        (project) => project.id !== id
+      );
+      return {
+        projects: remaningProject,
+        selectedProjectId: undefined,
+      };
+    });
+  }
+  function handleSelectProject(id) {
+    setProjectState((prev) => {
+      return {
+        ...prev,
+        selectedProjectId: id,
+      };
+    });
+  }
+  // function ProjectDetail() {
+  //   setProjectState((prev) => {
+  //     return {
+  //       ...prev,
+  //       selectedProjectId: undefined,
+  //     };
+  //   });
+  // }
+  console.log(projectState);
+  const selectedProject = projectState.projects.find(
+    (project) => project.id === projectState.selectedProjectId
+  );
+  let content = (
+    <ProjectSelected
+      projects={selectedProject}
+      selectProjectId={projectState.selectedProjectId}
+      deleteSelectedProject={deleteProject}
+    />
+  );
+  if (projectState.selectedProjectId === null) {
+    content = (
+      <NewProject
+        saveNewProject={addNewProject}
+        projecctDetails={projectState}
+        cancleProjectSave={cancleProject}
+      />
+    );
+  } else if (projectState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
+  }
+
   return (
     <>
       <main className=" flex gap-8 h-screen my-8">
-        <ProjectSideBar onSelect={handleAddProjectButn} />
-        {addProject > 0 ? (
-          <NewProject />
-        ) : (
-          <NoProjectSelected onSelect={handleAddProjectButn} />
-        )}
+        <ProjectSideBar
+          onStartAddProject={handleStartAddProject}
+          menuContent={projectState.projects}
+          onSelectProject={handleSelectProject}
+        />
+        {content}
       </main>
     </>
   );
--- ProjectManagementApp/src/component/ProjectSideBar.jsx
+++ ProjectManagementApp/src/component/ProjectSideBar.jsx
@@ -1,5 +1,10 @@
 import Button from "./Button";
-export default function ProjectSideBar({ onSelect }) {
+export default function ProjectSideBar({
+  onStartAddProject,
+  menuContent,
+  onSelectProject,
+  selectProjectId,
+}) {
   return (
     <>
       <aside className="w-1/3 px-8 py-16 bg-stone-900 text-stone-50 md:w-72 rounded-r-xl">
@@ -7,9 +12,29 @@ export default function ProjectSideBar({ onSelect }) {
           Your Projects
         </h2>
         <div>
-          <Button onClick={onSelect}>+ Add Project</Button>
+          <Button onClick={onStartAddProject}>+ Add Project</Button>
         </div>
-        <ul></ul>
+        <ul className="mt-8">
+          {menuContent.map((menuItems) => {
+            let cssClases =
+              "w-full text-left px-2 py-1 rounded-sm my-1 hover:text-stone-200 hover:bg-stone-800";
+            if (menuItems.id === selectProjectId) {
+              cssClases = cssClases + " bg-stone-800 text-stone-200";
+            } else {
+              cssClases = cssClases + " text-stone-400";
+            }
+            return (
+              <li key={menuItems.id}>
+                <button
+                  onClick={() => onSelectProject(menuItems.id)}
+                  className={cssClases}
+                >
+                  {menuItems.title}
+                </button>
+              </li>
+            );
+          })}
+        </ul>
       </aside>
     </>
   );
