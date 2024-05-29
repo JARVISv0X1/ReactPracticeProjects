4b8c4f4102f5dae2236cfdbef351fea9cc8c9fdd
Karan Kumar <karan.kumar@cashlesso.com>
2024-05-29 15:43:18 +0530

    Added signup page and aded validation in login form

--- reactForms/src/components/Login.jsx
+++ reactForms/src/components/Login.jsx
@@ -1,17 +1,55 @@
+import { useRef, useState } from "react";
+
 export default function Login() {
+  const [loginData, setLoginData] = useState({
+    email: "",
+    password: "",
+  });
+
+  function formSubmit(e) {
+    e.preventDefault();
+    console.log(loginData);
+  }
+  function validateInput(e) {
+    let { name, value } = e.target;
+
+    setLoginData((prev) => {
+      return {
+        ...prev,
+        [name]: value,
+      };
+    });
+  }
+  const emailIsInValid =
+    loginData.email !== "" && !loginData.email.includes("@");
   return (
-    <form>
+    <form onSubmit={formSubmit}>
       <h2>Login</h2>
 
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" />
+          <input
+            id="email"
+            type="email"
+            name="email"
+            onChange={validateInput}
+            value={loginData.email}
+          />
+          <div className="control-error">
+            {emailIsInValid && <p>Email should contain @</p>}
+          </div>
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
+          <input
+            id="password"
+            type="password"
+            name="password"
+            onChange={validateInput}
+            value={loginData.password}
+          />
         </div>
       </div>
 
